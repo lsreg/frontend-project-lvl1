9ec68c2e010af3b7702362ab81b8077ce52d2b73
lsreg <ls.izhevsk@gmail.com>
2021-11-18 02:50:26 +0300

    add brain-progression

---
+++ src/games/brain-progression.js
@@ -0,0 +1,25 @@
+import random from 'random';
+import runGame from '../game-engine.js';
+
+function generateQuestion() {
+  const first = random.int(1, 20);
+  const delta = random.int(1, 20);
+  const count = random.int(5, 10);
+  const missing = random.int(1, count);
+  let questionString = '';
+
+  for (let i = 1; i <= count; i++) {
+    if (i === missing) {
+      questionString += '.. ';
+    } else {
+      questionString += `${first + delta * (i - 1)} `;
+    }
+  }
+
+  return {
+    question: questionString.trim(),
+    answer: (first + (missing - 1) * delta).toString(),
+  };
+}
+
+runGame(generateQuestion);
